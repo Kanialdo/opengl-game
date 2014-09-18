23bab80d4f6a76f92c9e782473c5df372e773b6c
Krystian <kanialdo@gmail.com>
2014-09-19 01:14:49 +0200

    Dodane wstępne funkcje do korzystania z prawdziwego rzutu perspektywicznego

--- OpenGLGame/src/pl/krystiankaniowski/openglgame/renderer/AirHockeyRenderer.java
+++ OpenGLGame/src/pl/krystiankaniowski/openglgame/renderer/AirHockeyRenderer.java
@@ -15,7 +15,8 @@ import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
-import static android.opengl.Matrix.orthoM;
+import static android.opengl.Matrix.setIdentityM;
+import static android.opengl.Matrix.translateM;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -25,10 +26,14 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import pl.krystiankaniowski.openglgame.R;
+import pl.krystiankaniowski.openglgame.utils.MatrixHelper;
 import pl.krystiankaniowski.openglgame.utils.ShaderHelper;
 import pl.krystiankaniowski.openglgame.utils.TextResourceReader;
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
+import static android.opengl.GLES20.*;
+import static android.opengl.GLUtils.*;
+import static android.opengl.Matrix.*;
 
 public class AirHockeyRenderer implements Renderer {
 
@@ -44,7 +49,7 @@ public class AirHockeyRenderer implements Renderer {
 
 	private static final int BYTES_PER_FLOAT = 4;
 	private static final int COLOR_COMPONENT_COUNT = 3;
-	private static final int POSITION_COMPONENT_COUNT = 4;
+	private static final int POSITION_COMPONENT_COUNT = 2;
 	private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
 	private final Context context;
@@ -55,6 +60,7 @@ public class AirHockeyRenderer implements Renderer {
 	// =========================================================================
 
 	private final float[] projectionMatrix = new float[16];
+	private final float[] modelMatrix = new float[16];
 
 	private int program;
 
@@ -62,29 +68,24 @@ public class AirHockeyRenderer implements Renderer {
 	private int aPositionLocation;
 	private int uMatrixLocation;
 
-	float[] tableVerticesWithTriangles = {
-			// Order of coordinates: X, Y, Z, W, R, G, B
-			// Triangle Fan
-			0f, 0f, 0f, 1.5f, 1f, 1f, 1f, -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f, 0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f, 0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f, -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
-			-0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-			// Line 1
-			-0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f, 0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
-			// Mallets
-			0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f, 0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f };
-
 	// =========================================================================
 	// ----- KONSTRUKTOR -------------------------------------------------------
 	// =========================================================================
+
+	float[] tableVerticesWithTriangles = {
+			// Order of coordinates: X, Y, R, G, B
+			// Triangle Fan
+			0f, 0f, 1f, 1f, 1f, -0.5f, -0.8f, 0.7f, 0.7f, 0.7f, 0.5f, -0.8f, 0.7f, 0.7f, 0.7f, 0.5f, 0.8f, 0.7f, 0.7f, 0.7f, -0.5f, 0.8f, 0.7f, 0.7f, 0.7f, -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+			// Line 1
+			-0.5f, 0f, 1f, 0f, 0f, 0.5f, 0f, 1f, 0f, 0f,
+			// Mallets
+			0f, -0.4f, 0f, 0f, 1f, 0f, 0.4f, 1f, 0f, 0f };
 
 	public AirHockeyRenderer(Context context) {
 		this.context = context;
 		vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
 		vertexData.put(tableVerticesWithTriangles);
 	}
-
-	// =========================================================================
-	// ----- LOGIKA ------------------------------------------------------------
-	// =========================================================================
 
 	@Override
 	public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
@@ -149,16 +150,30 @@ public class AirHockeyRenderer implements Renderer {
 		// Set the OpenGL viewport to fill the entire surface.
 		glViewport(0, 0, width, height);
 
+		MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width / (float) height, 1f, 10f);
+
+		// to odsuwa macierz o 2 od punktu widzenia, dziêki czemu mo¿na zobaczyæ
+		// obraz
+		setIdentityM(modelMatrix, 0);
+		translateM(modelMatrix, 0, 0f, 0f, -2f);
+
+		final float[] temp = new float[16];
+		multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
+		System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+
 		// MACIERZ ORTOGONALNA
 
-		final float aspectRatio = width > height ? (float) width / (float) height : (float) height / (float) width;
-		if (width > height) {
-			// Landscape
-			orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
-		} else {
-			// Portrait or square
-			orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
-		}
+		// final float aspectRatio = width > height ? (float) width / (float)
+		// height : (float) height / (float) width;
+		// if (width > height) {
+		// // Landscape
+		// orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f,
+		// 1f);
+		// } else {
+		// // Portrait or square
+		// orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f,
+		// 1f);
+		// }
 
 	}
 
