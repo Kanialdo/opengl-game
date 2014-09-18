cdf970ad10aad0d2e4fbbf481f585657c352234c
Krystian <kanialdo@gmail.com>
2014-09-19 01:13:43 +0200

    Dodana klasa pzawierająca statyczne importy do kopiowania

---
+++ OpenGLGame/src/pl/krystiankaniowski/openglgame/dev/Backup.java
@@ -0,0 +1,9 @@
+package pl.krystiankaniowski.openglgame.dev;
+
+import static android.opengl.GLES20.*;
+import static android.opengl.GLUtils.*;
+import static android.opengl.Matrix.*;
+
+public class Backup {
+
+}
